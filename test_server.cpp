f69469b61b926635465018ff60916c38b43f1f0c
yuningyun <ing6264@naver.com>
2022-11-30 23:07:44 +0900

    221130_3

--- test_server.cpp
+++ test_server.cpp
@@ -83,7 +83,7 @@ int main(int argc, char* argv[])
 		int str_len;
 		char msg[1+NAME_SIZE+BUF_SIZE];
 		clnt_adr_sz = sizeof(clnt_adr);
-		clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
+		clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, (socklen_t*)&clnt_adr_sz);
 
 		pthread_mutex_lock(&mutx);
 		clnt_socks[clnt_cnt] = clnt_sock;
